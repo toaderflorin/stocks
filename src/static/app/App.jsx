776da8b12f8511966890524174ea24ce987da1f4
toaderflorin <toaderflorin@gmail.com>
2017-10-26 18:40:32 +0300

    Fixed typo.

--- src/static/app/App.jsx
+++ src/static/app/App.jsx
@@ -326,7 +326,7 @@ export default class App extends Component {
           <p>
             Hover over a candle to see details. Select two candles to zoom in to a time span,
             click Reset to reset the zoom level. The application shows values only for weekdays (Monday to Friday),
-            while show projection creates a prediction graph with the estimated values of the next 21 workdays.
+            while Show Projection creates a prediction graph with the estimated values of the next 21 workdays.
           </p>
           <p>
             Company
